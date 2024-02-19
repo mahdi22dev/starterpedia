997497ced125ad2a78391d692cebd24c576d914a
Mahdi idrissi <idrissimahdi2020@gmail.com>
2024-02-19 19:36:51 +0100

    complete footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export function Footer(): JSX.Element {
-  return <div className="bg-primary w-full h-24 p-4">footer a</div>;
+  return (
+    <div className="bg-primary/80 text-white ">
+      <div className="flex gap-10 items-center p-3">
+        <div>
+          <h1 className="text-4xl">Starterpedia</h1>
+          <p>welcome to starterpedia</p>
+        </div>
+        <ul className="md:flex ">
+          <li className="ml-4">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="ml-4">
+            <Link href="/resources">Resources</Link>
+          </li>
+          <li className="ml-4">
+            <Link href="/privacy">Privacy terms</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="w-full bg-primary flex justify-center items-center p-2">
+        &copy; 2024 Starterpedia.com
+      </div>
+    </div>
+  );
 }
